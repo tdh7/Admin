b09ff0425cde3fc4d3e141351c0759423e1c8859
trungdinhle <33343210+trungdinhle@users.noreply.github.com>
2019-04-22 23:34:12 +0700

    add index page

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+var app_name ="EleFind Administration"
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: app_name });
 });
 
-router.get()
 module.exports = router;
